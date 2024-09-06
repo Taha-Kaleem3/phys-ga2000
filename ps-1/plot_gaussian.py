d0a236160072c8e598ea861f64f67724a45e65eb
Taha-Kaleem <118640108+Taha-Kaleem@users.noreply.github.com>
2024-09-06 11:35:58 -0400

    fixing path to png file

--- ps-1/plot_gaussian.py
+++ ps-1/plot_gaussian.py
@@ -6,7 +6,7 @@ def gaussian(xrange, mean = 0, stddev = 1):
     Returns a normalized gaussian centered at a default mean of 0 and with a default standard deviation
     of 1. xrange is a numpy array that specifies the x values we want to plot.
     """
-    return np.exp(-np.power(np.subtract(xrange, mean),2)/(np.multiply(2,stddev**2)))
+    return (np.exp(-np.power(np.subtract(xrange, mean),2)/(np.multiply(2,stddev**2)))) * (1/(np.sqrt(2*np.pi)*stddev))
 
 # plotting 50 x and y values of the gaussian
 xrange = np.linspace(-10,10, 50, endpoint = True)
@@ -20,4 +20,4 @@ ax.set_title("Gaussian with mean 0 and standard deviation of 3")
 ax.set_xlabel("x")
 ax.set_ylabel("y")
 
-plt.savefig(fname = "C:\\Users\\kalee\\Documents\\GitHub\\phys-ga2000\\plot_gaussian")
+plt.savefig(fname = "C:\\Users\\kalee\\Documents\\GitHub\\phys-ga2000\\ps-1\\plot_gaussian")
