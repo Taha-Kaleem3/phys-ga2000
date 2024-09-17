c0a92d69acc2747aca180de70a0e6d8e1ca6d56c
Taha-Kaleem <tk604@scarletmail.rutgers.edu>
2024-09-16 22:34:03 -0400

    finished problems 4 and almost done with 5

---
+++ ps-2/quadratic.py
@@ -0,0 +1,12 @@
+import numpy as np
+def quadraticNormal(a, b, c):
+    return np.float32((-b+np.sqrt(b ** 2 - 4 * a *c))/(2*a)), np.float32((-b-np.sqrt(b ** 2 - 4 * a *c))/(2*a))
+
+def quadraticInverted(a, b, c):
+    return np.float32((2*c)/(-b-np.sqrt(b **2 -4*a*c))), np.float32((2*c)/(-b+np.sqrt(b **2 -4*a*c)))
+print(quadraticNormal(0.001, 1000, 0.001))
+print(quadraticInverted(0.001, 1000, 0.001))
+
+def quadratic(a, b, c):
+    return np.float32((-b+np.sqrt(b ** 2 - 4 * a *c))/(2*a)), np.float32((2*c)/(-b+np.sqrt(b **2 -4*a*c)))
+print(quadratic(0.001, 1000, 0.001))
