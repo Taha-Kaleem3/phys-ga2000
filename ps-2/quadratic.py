a08129d89ae7957766284c3bcfe17dcd97e07e70
Taha-Kaleem <118640108+Taha-Kaleem@users.noreply.github.com>
2024-09-17 10:48:30 -0400

    done with ps-2

--- ps-2/quadratic.py
+++ ps-2/quadratic.py
@@ -8,5 +8,8 @@ print(quadraticNormal(0.001, 1000, 0.001))
 print(quadraticInverted(0.001, 1000, 0.001))
 
 def quadratic(a, b, c):
-    return np.float32((-b+np.sqrt(b ** 2 - 4 * a *c))/(2*a)), np.float32((2*c)/(-b+np.sqrt(b **2 -4*a*c)))
+    if(b>=0):
+        return np.float32((2*c)/(-b-np.sqrt(b **2 -4*a*c))), np.float32((-b-np.sqrt(b ** 2 - 4 * a *c))/(2*a))
+    if (b<0):
+        return np.float32((-b+np.sqrt(b ** 2 - 4 * a *c))/(2*a)), np.float32((2*c)/(-b+np.sqrt(b **2 -4*a*c)))
 print(quadratic(0.001, 1000, 0.001))
